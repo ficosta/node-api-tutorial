dc874ba169b1a43e204d7b075d8d01491ca6aa01
Felipe <felipeiasi@gmail.com>
2019-12-19 16:03:35 -0300

    Instalação Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+//Iniciano o App
 const app = express()
-const port = 3001
+
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/node-api', 
+    {useNewUrlParser: true, useUnifiedTopology: true }
+)
+
 
 app.get('/', (req, res) => {
     res.send('Rocket')
 })
-app.listen(port, () => console.log(`Example app listening on {port} port!`))
+app.listen(3001, () => console.log(`Example app listening on {port} port!`))
